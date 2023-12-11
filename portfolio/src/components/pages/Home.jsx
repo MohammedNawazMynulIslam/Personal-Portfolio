a70d7ea0f2df70a8d9ee734af969ad88681c233b
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-12-11 14:51:11 +0600

    added movement in the island

--- portfolio/src/components/pages/Home.jsx
+++ portfolio/src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../Loader";
 import Island from "../../models/Island";
 import { Sky } from "../../models/Sky";
+import { Bird } from "./Bird";
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [6, -6.5, -43];
@@ -20,18 +22,23 @@ const Home = () => {
   return (
     <section className="w-full h-screen relative">
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader></Loader>}>
           <directionalLight position={[5, 5, 5]} intensity={1.5} />
           <ambientLight intensity={1} />
           <hemisphereLight skyColor="#b1e1ff " groundColor="#90ee90" />
+          <Bird />
           <Sky />
           <Island
             scale={islandScale}
             position={islandPosition}
             rotation={rotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
           />
         </Suspense>
       </Canvas>
